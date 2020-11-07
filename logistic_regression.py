1fbed0581b91bf2690c0994ae88f5bf6609b4d17
hanubv <bhanu.vankadari@gmail.com>
2020-11-07 19:25:04 +0530

    First Commit

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,8 +1,18 @@
+#importing required libraries 
+
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("/home/bhanu/Desktop/logic_data_1.txt",delimiter = ',')
+
+#loading the input data using numpy
+
+data = np.loadtxt("/home/bhanu/Desktop/codes/logic_data_1.txt",delimiter = ',')
+
+print(data)
+
+
+#splitting the data
 
 x1 = data[:,0]
 
@@ -10,19 +20,27 @@ x2 = data[:,1]
 
 y  = data[:,2]
 
-#print(data)
-#print(x2)
-#print(x1)
 
-m1	= 0.5
-m2	= 0.5
-c	= 0.3
-alpha	= 0.1
-iterations = 1000
+#Training Parameters 
+
+m1	= 0.5		#assuming weight-m1
+
+m2	= 0.5		#assuming weight-m2
+
+c	= 0.3		#assuming bias - c
+
+alpha	= 0.1		#assuming learning rate(alpha)
+
+iterations = 1000	#Taking number of iterations to train the model
+
+
+#input data contains larger values so to get better results
+#Normalizing the input data to get better output by using "mean normalization" method
 
 x1_mean	= sum(x1)/len(x1)
 
 x1_max	= max(x1)
+
 x1_min	= min(x1)
 
 
@@ -30,20 +48,24 @@ x1_min	= min(x1)
 x2_mean	= sum(x2) / len(x2)
 
 x2_max	= max(x2)
+
 x2_min	= min(x2)
 
+#mean normalization of x1 and x2 data
 x1_mean_data	= (x1 - x1_mean) / (x1_max - x1_min)
+
 x2_mean_data	= (x2 - x2_mean) / (x2_max - x2_min)
 
-print("x1_mean values\n:")
-print(x1_mean_data)	
 
-print("\nx2_mean values:\n")
-print(x2_mean_data)
 
 loss=[]
 
+#looping through the entire dataset to train the model
+
 for i in range(iterations):
+
+	
+	#finding y_predicted value using sigmoid activation function
 
 	z	= m1*x1_mean_data + m2*x2_mean_data + c
 	
@@ -52,6 +74,9 @@ for i in range(iterations):
 	m	= 1+exp
 
 	y_pred	= 1. / m
+	
+
+	#finding x1, x2, c gradients
 
 	grad_x1	= x1_mean_data*(y_pred - y)
 
@@ -67,10 +92,13 @@ for i in range(iterations):
 
 	grad_c	= 1./len(grad_c) * sum(grad_c)
 
+	#updating the weights m1,m2
+	
 	m1	= m1 - alpha * grad_x1
 	
 	m2	= m2 - alpha * grad_x2
 	
+	#updating the bias value c
 	c	= c  - alpha * grad_c
 	
 	log_1	= y*np.log(y_pred)
@@ -83,9 +111,6 @@ for i in range(iterations):
 
 
 iter_list = range(0,iterations)
-#plt.plot(iter_list,loss)
-#plt.show()
-
 
 pred_y = m1*x1_mean_data + m2*x2_mean_data + c
 
@@ -93,8 +118,8 @@ classes =[1 if logit>0.5 else 0 for logit in pred_y]
 
 slope = -m2/m1
 
-#plt.plot(x1_mean_data,x1*slope)
-#plt.hold(True)
+
+#scattering the data
 plt.scatter(x1_mean_data,x2_mean_data,c=classes)
 
 plt.show()
@@ -103,7 +128,7 @@ diff = classes - y
 
 accuracy = 1.0 - (float(np.count_nonzero(diff)) / len(diff))*1.0
 
-
+#printing the weights and bias values
 print(m1,m2,c)
 
 
